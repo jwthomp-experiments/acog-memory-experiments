94f874decc9458ef7f533e74a6cf69536ee76d5e
Jeff Thompson <mythrndr@gmail.com>
2023-04-04 21:27:45 -0500

    Update main.py to use qdrant

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
 import signal
 import sys
-import uuid4
-
+from uuid import uuid4
 import torch
 #from transformers import AutoTokenizer, AutoModelForCausalLM
 from sentence_transformers import SentenceTransformer
-
-from pymilvus import (
-    connections,
-    utility,
-    FieldSchema, CollectionSchema, DataType,
-    Collection,
-)
-
-
+from qdrant_client import QdrantClient
+from qdrant_client import models
 
 TOKENIZER = None
 MODEL = None
 SENTENCE_MODEL = None
+QDRANT_CLIENT = None
 
 def sigint_handler(signal, frame):
     print("\nBye now!")
@@ -25,10 +18,12 @@ def sigint_handler(signal, frame):
 
 signal.signal(signal.SIGINT, sigint_handler)
 
+
 def initialize():
     global TOKENIZER
     global MODEL
     global SENTENCE_MODEL
+    global QDRANT_CLIENT
 
     SENTENCE_MODEL = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
@@ -39,48 +34,86 @@ def initialize():
     #    load_in_8bit=True, 
     #    device_map="auto")
 
-    # Connect to Milvus Vector Database
-    connections.connect("default", host="localhost", port="19530")
+    # Connect to Qdrant
+    QDRANT_CLIENT = QdrantClient(host='localhost', port=6333)
+    if does_collection_exist("sentences") is False:
+        QDRANT_CLIENT.recreate_collection(
+            collection_name='sentences',
+            vectors_config=models.VectorParams(size=384, distance=models.Distance.COSINE)
+        )
 
-    schema = CollectionSchema(
-        fields = [
-            FieldSchema(
-                name = "id",
-                dtype = DataType.VARCHAR,
-                is_primary = True,
-                max_length = 36
-            ),
-            FieldSchema(
-                name = "speaker",
-                dtype = DataType.VARCHAR,
-                max_length = 64
-            ),
-            FieldSchema(
-                name = "message",
-                dtype = DataType.FLOAT_VECTOR,
-                dim=384
+
+def does_collection_exist(collection_name):
+    collections = QDRANT_CLIENT.get_collections()
+    print(f"Collections: {collections.collections}")
+    for collection in collections.collections:
+        print(f"Collection: {collection}")
+        if collection.name == collection_name:
+            return True
+    return False
+
+
+def get_sentence_embeddings(input):
+    embeddings = SENTENCE_MODEL.encode(input)
+    #print(embeddings)
+    #print(f"Length: {len(embeddings)}")
+
+    return embeddings
+
+
+def store_embeddings(embeddings, inputs):
+    print(f" len emb: {len(embeddings)} len inputs: {len(inputs)}")
+    QDRANT_CLIENT.upsert(
+        collection_name = 'sentences',
+        points = [
+            models.PointStruct(
+                id = str(uuid4()),
+                vector = vector.tolist(),
+                payload = {
+                    "message": inputs[idx]
+                }
             )
+            for idx, vector in enumerate(embeddings)
         ]
     )
 
-def process_text(input):
+
+def search(embeddings):
+    search_result = QDRANT_CLIENT.search(
+        collection_name="sentences",
+        query_vector = embeddings[0],
+        query_filter = None,
+        limit = 5
+    )
+    return [hit.payload for hit in search_result]
+
+
+def process_input(input):
     output = "Hi Choom"
-
-    embeddings = SENTENCE_MODEL.encode(input)
-    print(embeddings)
-    print(f"Length: {len(embeddings[0])}")
-
     return output
+
+
+def cli_get_input():
+    return input("You: ")
+
 
 def main():
     initialize()
 
     while(True):
-        text = input("You: ")
-        embeddings = get_sentence_embeddings(text)
-        unique_id = str(uuid4())
-        output = process_text([text])
+        # Get User Input
+        input = cli_get_input()
+
+        # Convert input into embeddings and store it
+        embeddings = get_sentence_embeddings([input])
+        store_embeddings(embeddings, [input])
+        output = process_input(input)
         print(f"Miyu: {output}")
+
+        payloads = search(embeddings)
+
+        for idx, payload in enumerate(payloads):
+            print(f"{idx}: {payload}")
 
 
 if __name__ == "__main__":
